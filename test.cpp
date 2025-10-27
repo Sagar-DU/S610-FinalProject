62d683d6aaa168724b6d1b8b3b7730e5ebe9b0b4
Nahid Hasan Sagar <nsagar@i42.red.uits.iu.edu>
2025-10-27 18:03:24 -0400

    test loop added

--- test.cpp
+++ test.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
     cout << "Bismillah ArRahmanir Rohim!" << endl;
+
+    int sum = 0;
+    for (int i = 1; i <= 100; ++i) {
+        sum += i;
+    }
+    
+    cout << "Sum from 1 to 100 is: " << sum << endl;
     return 0;
 }
