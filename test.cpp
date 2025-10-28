bbbd330ef61241d5b5bf637725ac8e82ce3bd6c7
nahidhasansagar@gmail.com <nsagar@i42.red.uits.iu.edu>
2025-10-28 13:55:26 -0400

    updated the test file to test gihub from the supercomputer

--- test.cpp
+++ test.cpp
@@ -8,8 +8,9 @@ int main() {
     int sum = 0;
     for (int i = 1; i <= 100; ++i) {
         sum += i;
+        cout << "Adding " << i << ", current sum: " << sum << endl;
     }
-    
+
     cout << "Sum from 1 to 100 is: " << sum << endl;
     return 0;
 }
